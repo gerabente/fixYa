1b2624df4815d64d8656b1b4d8501394f281410b
Totema26 <estebantotema@gmail.com>
2025-06-26 11:58:29 -0300

    Nuevos metodos en Estado y orden (tote)

--- src/main/java/com/mycompany/fixya/Estado.java
+++ src/main/java/com/mycompany/fixya/Estado.java
@@ -5,5 +5,10 @@ package com.mycompany.fixya;
  * @author PC
  */
 public enum Estado {
-    Espera, Proceso, Terminado
+    Espera, Proceso, Terminado;
+    
+    // Método para mostrar el estado del vehículo
+    public String mostrarEstado() {
+        return "El vehículo está en " + this.name(); // Usar this.name() para obtener el nombre del enum
+    }
 }
