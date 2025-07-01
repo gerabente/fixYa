179a0014ac3b69b340fb1ecec6de280e6b7c7d7a
PC <gerabente@gmail.com>
2025-07-01 09:17:58 -0400

    Nuevo JFRAME

--- src/main/java/com/mycompany/fixya/Autenticacion.java
+++ src/main/java/com/mycompany/fixya/Autenticacion.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.fixya;
 
-/**
- *
- * @author PC
- */
 public class Autenticacion {
+    private UsuarioDAO usuarioDAO;
     
+    public Autenticacion(){
+        this.usuarioDAO = new UsuarioDAO();
+    }
+    
+    public boolean registrarUsuario(String nombre, String correo, String contrasena){
+        
+    }
 }
