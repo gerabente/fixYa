646f2abafdaad15608fdc99424e3d14db952d0e0
Totema26 <estebantotema@gmail.com>
2025-07-02 10:00:47 -0400

    Creacion de JFrames 2

--- src/main/java/com/mycompany/fixya/Vehiculos.java
+++ src/main/java/com/mycompany/fixya/Vehiculos.java
@@ -8,12 +8,12 @@ package com.mycompany.fixya;
  *
  * @author Tote
  */
-public class AgregarVisita extends javax.swing.JFrame {
+public class Vehiculos extends javax.swing.JFrame {
 
     /**
-     * Creates new form AgregarVisita
+     * Creates new form Vehiculos
      */
-    public AgregarVisita() {
+    public Vehiculos() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class AgregarVisita extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AgregarVisita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Vehiculos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AgregarVisita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Vehiculos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AgregarVisita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Vehiculos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AgregarVisita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Vehiculos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AgregarVisita().setVisible(true);
+                new Vehiculos().setVisible(true);
             }
         });
     }
